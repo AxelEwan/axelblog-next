30bbeabd25b4e63a7b604a68ff0432a4c4762775
Axel <139603893+ErenAxel@users.noreply.github.com>
2026-03-15 23:29:27 +0800

    Update BlogLog.vue

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -2,10 +2,11 @@
 const { timeEstablished } = useAppConfig()
 
 const blogLog = [
-	{ label: '2025-07-26', value: '重构到 Nuxt 4 + Nuxt Content v3，启用 zhilu.site 域名' },
-	{ label: '2024-08-11', value: '重构到 Nuxt 3 + Nuxt Content v2' },
-	{ label: '2023-05-24', value: '更换框架为 Hexo，升级博客' },
-	{ label: '2020-08-24', value: '使用 zhilu.cyou 域名' },
+	{ label: '2026-03', value: '重构到 Nuxt 4 + Nuxt Content v3，全面静态' },
+	{ label: '2025-01', value: '转回 Halo，优化博客' },
+	{ label: '2024-05', value: '性能受限+误删，转回WordPress，启用新主题' },
+	{ label: '2024-02', value: '更换框架为 Halo，升级博客' },
+	{ label: '2023-11', value: '使用 Axelx.cn 域名，使用WordPress框架' },
 	{ label: timeEstablished, value: '发布第一篇文章' },
 ]
 </script>
